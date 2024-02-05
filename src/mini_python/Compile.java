00920ecd90de588035fd8763b10ec4304d3bc09a
Luc <luc.buysse@polytechnique.edu>
2024-02-05 16:31:27 +0100

    Add

--- src/mini_python/Compile.java
+++ src/mini_python/Compile.java
@@ -5,6 +5,72 @@ class Compile {
   static boolean debug = false;
 
   static X86_64 file(TFile f) {
-    return null; // TODO
+    return null;
+  }
+}
+
+class MyVisitor implements TVisitor {
+  public void visit(Cnone c) {
+
+  }
+  public void visit(Cbool c) {
+
+  }
+  public void visit(Cstring c) {
+
+  }
+  public void visit(Cint c) {
+
+  }
+  public void visit(TEcst e)  {
+
+  }
+  public void visit(TEbinop e)  {
+
+  }
+  public void visit(TEunop e) {
+
+  }
+  public void visit(TEident e) {
+
+  }
+  public void visit(TEcall e) {
+
+  }
+  public void visit(TEget e) {
+
+  }
+  public void visit(TElist e) {
+
+  }
+  public void visit(TErange e) {
+
+  }
+  public void visit(TSif s) {
+
+  }
+  public void visit(TSreturn s) {
+
+  }
+  public void visit(TSassign s) {
+
+  }
+  public void visit(TSprint s) {
+
+  }
+  public void visit(TSblock s) {
+
+  }
+  public void visit(TSfor s) {
+
+  }
+  public void visit(TSeval s) {
+
+  }
+  public void visit(TSset s) {
+
+  }
+  public void visit(TDef d) {
+
   }
 }
